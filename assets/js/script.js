15add13dff8ee8134e6a5f89aba9889a68414f7c
Drew Selden <seldend1@gmail.com>
2021-05-23 00:18:38 -0400

    switched old index.html and script.js with working ones and renamed working ones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,174 +1,154 @@
-  /* First attempt : couldn't figure how to bring up highscore/submit feature */
-// Went on making 3 files of each html/css/js 
+// Calling global variables
 
-// var startButton = document.querySelector("#start-btn")
-// var vhsButton = document.querySelector("#vhs-btn")
-// var questionBoxEl = document.querySelector("#question-box")
-// var homeEl = document.querySelector(".home")
-// var countdownEl = document.querySelector("#countdown")
-// var questionEl = document.querySelector("#question")
-// var answerButtonsEl = document.querySelector("#answer-buttons")
-// var timerDisplay = document.querySelector('#timerDisplay');
-// var timeLeft = 75
-// let shuffledQuestions, currentQuestionIndex
-// let currentQuestions = {}
+var question = document.querySelector("#question")
+var startButton = document.querySelector("#start-btn")
+var choices = Array.from(document.querySelectorAll(".option-text"));
+var questionProgress = document.querySelector("#questionProgress")
+var timerDisplay = document.querySelector("#timerDisplay")
+var question = document.querySelector("#question")
+var time = 60
 
-// startButton.addEventListener('click', startGame);
-// // this next line will make all the questions go through
-// answerButtonsEl.addEventListener('click', () => {
-//   currentQuestionIndex++
-//   setNextQuestion()
-// })
+let currentQuestion = {}
+let acceptingAnswers = true
 
-// function startGame() {
-//   startButton.classList.add('hide')
-//   homeEl.classList.add('hide')
-//   questionBoxEl.classList.remove('hide')
-//   countdownEl.classList.remove('hide')
-//   shuffledQuestions = questions.sort(() => Math.random() -.5)
-//   currentQuestionIndex = 0
-//   availableQuestions = [...questions]
-//   setNextQuestion()
-// }
+let questionCounter = 0
+let availableQuestions = []
 
-//   function setNextQuestion() {
-//   resetState()
-//   showQuestion(shuffledQuestions[currentQuestionIndex])
-// }
-//   function showQuestion(question) {
-//   questionEl.innerText = question.question
-//   question.answers.forEach(answer => {
-//     var button = document.createElement('button');
-//     button.innerText = answer.text
-//     button.classList.add('btn')
-//     if (answer.correct) {
-//       button.dataset.correct = answer.correct
-//     }
-//     button.addEventListener('click', selectAnswer)
-//     answerButtonsEl.appendChild(button)
+let questions = [
+    {
+        question: "Which of the following is not JavaScript Data Types?",
+        choice1: "Undefined", 
+        choice2: "Number", 
+        choice3: "Boolean", 
+        choice4: "Float", 
+        answer: 4,
+    },
+    {
+        question: "Which company developed JavaScript? ",
+        choice1: "Netscape", 
+        choice2: "Bell Labs", 
+        choice3: "Sun Microsystems", 
+        choice4: "IBM", 
+        answer: 1,
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choice1: "<script>", 
+        choice2: "<head>",
+        choice3: "<meta>", 
+        choice4: "<style>",
+        answer: 1,
+    },
+    {
+        question: "Which built-in method returns the length of the string?",
+        choice1: "size()", 
+        choice2: "length()", 
+        choice3: "append()", 
+        choice4: "index()", 
+        answer: 2,
+    },
+    {
+        question: "Which of the following function of String object returns the calling string value converted to upper case?",
+        choice1: "toString()", 
+        choice2: "toLocaleUpperCase()", 
+        choice3: "substring()", 
+        choice4: "toUpperCase()", 
+        answer: 4,
+    }]
+    const TIMER = 10
+    const MAX_QUESTIONS = 5
+
+    startGame = function() {
+        questionCounter = 0
+        score = 0
+        availableQuestions = [...questions]
+        getNewQuestion();
+    }
+    // I could not figure out how to do it all on one html. //  
+    getNewQuestion = function() {
+        if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+            localStorage.setItem('mostRecentScore', score)
+
+            return window.location.assign('end.html')
+         }
+        questionCounter++
+        questionProgress.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}` 
+        // Grabbing random questions from the from the available amount of questions. //
+        var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+        currentQuestion = availableQuestions[questionsIndex];
+        question.innerText = currentQuestion.question
+
+        choices.forEach(choice => {
+            const number = choice.dataset['number']
+            choice.innerText = currentQuestion['choice' + number]
+        })
+        availableQuestions.splice(questionsIndex, 1)
+
+        acceptingAnswers = true
+    }
+    choices.forEach(choice => {
+        choice.addEventListener('click', e => {
+            if(!acceptingAnswers) return
+            acceptingAnswers = false
+            var selectedChoice = e.target
+            var selectedAnswer = selectedChoice.dataset['number']
+
+            // ternary operator 
+            let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+            
+            if(classToApply === 'correct') {
+                incrementTime(TIMER)
+            }
+            if(classToApply === 'incorrect') {
+                decrementTime(TIMER)
+            }
+
+            selectedChoice.parentElement.classList.add(classToApply)
+
+            setTimeout(() => {
+                selectedChoice.parentElement.classList.remove(classToApply)
+                getNewQuestion();
+            }, 500)
+
+        })
+    })
+    // + 10 points/timer for a correct answer
+    incrementTime = function(num) {
+        score +=num
+        timerDisplay.innerText = score    
+    }
+    // - 10 points/timer for incorrect
+    decrementTime = num => {
+        score -=num
+        timerDisplay.innerText = score 
+    }
+    // Couldn't get the score to add/subtract from the timer or to add the timer to the score for totalScore // 
+
+    // document.querySelectorAll('incorrect').addEventListener('click', function() {
+    //     time -= 10;
+    //     document.querySelector('#timerDisplay').innerHTML= time;
+    // });
+    function startTimer() {
+        var time = 75
+        var timeInterval = setInterval(function () {
+            time--;
+            timerDisplay.innerHTML = time;
+        if (time < 1) {
+          clearInterval(timeInterval);
+          alert("Time is up!")
+        }
+      }, 1000);
+      
+   }
+      startTimer();
+      
+
+       
+
+      startGame();
     
-    
-//   })
-// }
-// function startTimer() {
-//   var timeLeft = 75
-//   var timeInterval = setInterval(function () {
-//      timeLeft--;
-//     timerDisplay.innerHTML = timeLeft;
-//   if (timeLeft < 1) {
-//     clearInterval(timeInterval);
-//     alert("Time is up!")
-//   }
-// }, 1000);
-// }
 
-//   startButton.onclick = function(){
-//   startButton.disabled = true;
-//   startTimer();
-// }  
-
-// function resetState() {
-//   clearStatusClass(document.body)
-//   while (answerButtonsEl.firstChild) {
-//     answerButtonsEl.removeChild(answerButtonsEl.firstChild)
-//   }
-// }
-
-// function selectAnswer(event) {
-//   var selectedButton = event.target
-//   var correct = selectedButton.dataset.correct
-//   setStatusClass(document.body, correct)
-//   Array.from(answerButtonsEl.children).forEach(button => {
-//   setStatusClass(button, button.dataset.correct)
-//   })
-  
-//   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-  
     
-//   } else {
-//     questionBoxEl.classList.add('hide')
-//     startButton.classList.remove('hide')
-//     startButton.innerText = "Play Again"
-    
-    
-//   }
-// }
 
-// function setStatusClass(element, correct) {
-  
-//   clearStatusClass(element)
-//   if (correct) {
-//     element.classList.add('correct')
-//   } else {
-//     element.classList.add('incorrect')
-//     timeLeft = timeLeft - 10;
-//       timerDisplay.innerHTML = timeLeft
-//       answerButtonsEl.classList.remove('correct','incorrect')
      
-//       }
-// }
-// function clearStatusClass(element) {
-//   element.classList.remove('correct')
-//   element.classList.remove('incorrect')
-// }
-
-  
-    
-    
-
-//   //     var countdown = function(num) {
-//   //   for (var i = num; i > 0; i--) {
-//   //     console.log(i);
-//   //   }
-//   // };
-  
-// // startTimer();
-// // })();
-
-//   const questions = [
-//     {
-//         question: "Which of the following is not JavaScript Data Types?",
-//         answers: [
-//         { text: "Undefined", correct: false },
-//         { text: "Number", correct: false },
-//         { text: "Boolean", correct: false },
-//         { text: "Float", correct: true }
-//         ]
-//       },
-//       {
-//         question: "Which company developed JavaScript? ", 
-//         answers: [
-//         { text: "Netscape", correct: true },
-//         { text: "Bell Labs", correct: false },
-//         { text: "Sun Microsystems", correct: false },
-//         { text: "IBM", correct: false }
-//         ]
-//       },
-//       {
-//         question: "Inside which HTML element do we put the JavaScript?",
-//         answers: [
-//         { text: "<script>", correct: true },
-//         { text: "<head>", correct: false },
-//         { text: "<meta>", correct: false },
-//         { text: "<style>", correct: false }
-//         ]
-//       },
-//       {
-//         question: "Which built-in method returns the length of the string?",
-//         answers: [
-//         { text: "size()", correct: false },
-//         { text: "index()", correct: false },
-//         { text: "length()", correct: true },
-//         { text: "append()", correct: false } 
-//         ]
-//       },
-//       {
-//         question: "Which of the following function of String object returns the calling string value converted to upper case?",
-//         answers: [
-//         { text: "toString()", correct: false},
-//         { text: "toLocaleUpperCase()", correct: false},
-//         { text: "substring()", correct: false},
-//         { text: "toUpperCase()", correct: true}
-//         ]
-//       }
-//     ]
+     
